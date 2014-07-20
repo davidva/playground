8272bc5859c639616a9708d561e53984a08fd616
David Vilar <davidv@shutl.com>
2014-07-20 11:45:09 +0100

    gracefully shutting down

--- Actors/hello_actors/hello_actors.exs
+++ Actors/hello_actors/hello_actors.exs
@@ -4,14 +4,19 @@ defmodule Talker do
       {:greet, name} -> IO.puts("Hello #{name}")
       {:praise, name} -> IO.puts("#{name}, you're amazing")
       {:celebrate, name, age} -> IO.puts("Here's to another #{age} years, #{name}")
+      {:shutdown} -> exit(:normal)
     end
     loop
   end
 end
 
-pid = spawn(&Talker.loop/0)
+Process.flag(:trap_exit, true)
+pid = spawn_link(&Talker.loop/0)
 send(pid, {:greet, "Huey"})
 send(pid, {:praise, "Dewey"})
 send(pid, {:celebrate, "Louie", 16})
+send(pid, {:shutdown})
 
-:timer.sleep(1000)
+receive do
+  {:EXIT, ^pid, reason} -> IO.puts("Talker has exited (#{reason})")
+end
