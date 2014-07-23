61fed8dd9fb9750bc0707a8dd1113b2febd6098c
David Vilar <davidv@shutl.com>
2014-07-23 16:39:32 +0100

    keeping cache versions with and without using OTP

--- concurrency/Actors/cache/cache.ex
+++ concurrency/Actors/cache/cache.ex
@@ -1,46 +1,76 @@
 defmodule CacheSupervisor do
-  use Supervisor
-
-  def start_link do
-    :supervisor.start_link(__MODULE__, [])
+  def start do
+    spawn(__MODULE__, :loop_system, [])
   end
 
-  def init(_args) do
-    workers = [worker(Cache, [])]
-    supervise(workers, strategy: :one_for_one)
+  def loop do
+    pid = Cache.start_link
+    receive do
+      {:EXIT, ^pid, :normal} ->
+        IO.puts("Cache exited normally")
+        :ok
+
+      {:EXIT, ^pid, reason} ->
+        IO.puts("Cache failed with reason #{inspect reason} - restarting it")
+        loop
+    end
+  end
+
+  def loop_system do
+    Process.flag(:trap_exit, true)
+    loop
   end
 end
 
 defmodule Cache do
-  use GenServer
-
   def start_link do
-    :gen_server.start_link({:local, :cache}, __MODULE__, {HashDict.new, 0}, [])
+    pid = spawn_link(__MODULE__, :loop, [HashDict.new, 0])
+    Process.register(pid, :cache)
+    pid
   end
 
   def put(url, page) do
-    :gen_server.cast(:cache, {:put, url, page})
+    send(:cache, {:put, url, page})
   end
 
   def get(url) do
-    :gen_server.call(:cache, {:get, url})
+    ref = make_ref()
+    send(:cache, {:get, self(), ref, url})
+    receive do
+      {:ok, ^ref, page} -> page
+      after 1000 -> nil
+    end
   end
 
   def size do
-    :gen_server.call(:cache, {:size})
+    ref = make_ref()
+    send(:cache, {:size, self(), ref})
+    receive do
+      {:ok, ^ref, s} -> s
+      after 1000 -> nil
+    end
   end
 
-  def handle_cast({:put, url, page}, {pages, size}) do
-    new_pages = Dict.put(pages, url, page)
-    new_size = size + byte_size(page)
-    {:noreply, {new_pages, new_size}}
+  def terminate do
+    send(:cache, {:terminate})
   end
 
-  def handle_call({:get, url}, _from, {pages, size}) do
-    {:reply, pages[url], {pages, size}}
-  end
+  def loop(pages, size) do
+    receive do
+      {:put, url, page} ->
+        new_pages = Dict.put(pages, url, page)
+        new_size = size + byte_size(page)
+        loop(new_pages, new_size)
 
-  def handle_call({:size}, _from, {pages, size}) do
-    {:reply, size, {pages, size}}
+      {:get, sender, ref, url} ->
+        send(sender, {:ok, ref, pages[url]})
+        loop(pages, size)
+
+      {:size, sender, ref} ->
+        send(sender, {:ok, ref, size})
+        loop(pages, size)
+
+      {:terminate} -> # Terminate request - don't recurse
+    end
   end
 end
